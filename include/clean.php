96923045df8daf925b2eda8372580aab017a2378
Gregory93600 <1938269@cgmatane.qc.ca>
2022-04-10 22:16:36 -0400

    ajout du clean pour javascript

--- include/clean.php
+++ include/clean.php
@@ -86,3 +86,21 @@ add_action(
 		remove_action( 'wp_head', 'wp_resource_hints', 2 );
 	}
 );
+/**
+ * De-registers WordPress default javascript
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_deregister_script/
+ */
+add_action(
+	'wp_enqueue_scripts',
+	function () {
+		wp_deregister_script( 'jquery' );
+	}
+);
+
+/**
+ * Remove oEmbed-specific JavaScript from the front-end and back-end.
+ *
+ * @link https://developer.wordpress.org/reference/functions/wp_oembed_add_host_js/
+ */
+remove_action( 'wp_head', 'wp_oembed_add_host_js' );
