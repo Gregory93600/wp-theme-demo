aac731a46ba967feced0a22f28c5c42300646457
Gregory93600 <1938269@cgmatane.qc.ca>
2022-04-10 22:18:13 -0400

    ajout du clean REST API

--- include/clean.php
+++ include/clean.php
@@ -104,3 +104,17 @@ add_action(
  * @link https://developer.wordpress.org/reference/functions/wp_oembed_add_host_js/
  */
 remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+
+/**
+ * Disable the REST API
+ */
+add_action(
+	'init',
+	function () {
+		add_filter( 'rest_jsonp_enabled', '__return_false' );
+
+		remove_action( 'xmlrpc_rsd_apis', 'rest_output_rsd' );
+		remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+		remove_action( 'template_redirect', 'rest_output_link_header', 11 );
+	}
+);
